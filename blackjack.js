d8eee8164530168168ce07b3b0a94c854d49a169
Adolfo Aparicio <financieras@gmail.com>
2016-08-22 00:36:21 +0200

    Create blackjack.js

---
+++ blackjack.js
@@ -0,0 +1,13 @@
+// Simulador simplificado del juego de Blackjack
+
+// Objeto baraja, conjunto de cartas que tienen un palo y un valor
+var baraja=[];
+var palos=["c","p","t","d"];
+for(var i=0;i<palos.lenght;i++){
+  for(var j=1;j<=13;j++){
+    var carta={palo:palos[i],valor:j};
+    baraja[baraja.length]=carta;
+    document.write(carta);
+  }
+}
+
