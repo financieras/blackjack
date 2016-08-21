dda3f5fd451364d97544a14972b273159c927d4c
Adolfo Aparicio <financieras@gmail.com>
2016-08-22 01:11:44 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -7,7 +7,7 @@ for(var i=0;i<palos.length;i++){
   for(var j=1;j<=13;j++){
     var carta={palo:palos[i],valor:j};
     baraja[baraja.length]=carta;
-    document.write(palo[i]+j+"<br>";);
+    document.write(palos[i]+j+"<br>";);
   }
 }
 document.write("hola");
