44d3ce983c37d3b45d33ca5764f398e0d5ee054f
Adolfo Aparicio <financieras@gmail.com>
2016-08-22 01:11:02 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -3,11 +3,11 @@
 // Objeto baraja, conjunto de cartas que tienen un palo y un valor
 var baraja=[];
 var palos=["c","p","t","d"];
-for(var i=0;i<palos.lenght;i++){
+for(var i=0;i<palos.length;i++){
   for(var j=1;j<=13;j++){
     var carta={palo:palos[i],valor:j};
     baraja[baraja.length]=carta;
-    document.write(i+j);
+    document.write(palo[i]+j+"<br>";);
   }
 }
 document.write("hola");
