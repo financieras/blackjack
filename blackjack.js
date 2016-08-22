3659a837e46a33f7cc23c96efde41bad6c49cc90
Adolfo Aparicio <financieras@gmail.com>
2016-08-22 10:09:08 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -3,13 +3,13 @@ document.write('<h1>'+"Simulación básica del juego de Blackjack"+'</h1>');
 // Objeto baraja, conjunto de cartas que tienen un palo y un valor
 var baraja=[];
 var palos=["c","p","t","d"];
-var texto="";
 for(var i=0;i<palos.length;i++){
   for(var j=1;j<=13;j++){
     var carta={palo:palos[i],valor:j};
     baraja[baraja.length]=carta;
-    texto=texto+(j+palos[i]+" - ");
-    //document.write("\n"); permite un cambio de línea. equivale a <br> en html
+    document.write();
   }
 }
-document.write(texto.substring(0, texto.length-3));
+for(var j=1;j<=13;j++){
+  baraja[j];
+}
