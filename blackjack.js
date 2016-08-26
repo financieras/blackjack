7bee2a84a9925353c021b9494ed14be730e89f9b
Adolfo Aparicio <financieras@gmail.com>
2016-08-26 18:13:54 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,40 @@
 // Simulador simplificado del juego de Blackjack
 document.write('<h1>'+"Simulación básica del juego de Blackjack"+'</h1>'); 
-// Objeto baraja, conjunto de cartas que tienen un palo y un valor
-var baraja=[];
-var palos=["c","p","t","d"];
-for(var i=0;i<palos.length;i++){
-  for(var j=1;j<=13;j++){
-    var carta={palo:palos[i],valor:j};
-    baraja[baraja.length]=carta;
-    document.write();
+//Juego de Blackjack. Simulación básica del juego, programado para ejecutar en la consola
+
+                                                  //creación de variables globales
+var naipes=[];                                    //contendrá la baraja completa y desordenada
+var usadas=0;                                     //cartas usadas de entre las 52 de naipes
+var manoJ=[];                                     //vector con la mano del Jugado
+var manoC=[];                                     //vector con la mano del Crupier
+var jugando= false;                               //indica si estoy jugando o el juego ha terminado
+var totalJ=0;                                     //maxima puntuación del Jugador considerando el as como 1 o como 11
+var totalC=0;                                     //maxima puntuación del Croupier considerando el as como 1 o como 11
+
+
+function generaBaraja(){                          //Función que da naipes con 52 cartas barajeadas
+	var palos=["c","d","p","t"];                    //1º: generamos la baraja ordenada
+	for(i=0;i<palos.length;i++){
+		for(j=1;j<=13;j++){
+			 var carta={palo:palos[i],valor:j};
+			naipes[naipes.length]=carta;
+		}
+	}
+  for(k=1;k<=100;k++){                            //2º: ahora la barajeamos
+  	var una=Math.floor(52*Math.random());
+  	var otra=Math.floor(52*Math.random());
+  	var temp={};
+  	temp=naipes[una];
+  	naipes[una]=naipes[otra];
+  	naipes[otra]=temp;
   }
+	return naipes;
 }
-for(var j=0;j<13;j++){
-  baraja[j];
+
+function blackjack(){
+  document.write(naipes);  
 }
+
+
+
+blackjack();
