1113e0a3563b304f176b2de315cb42cd9824b1f0
Adolfo Aparicio <financieras@gmail.com>
2016-08-27 00:12:27 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -53,6 +53,7 @@ function blackjack(){
 
 function pideJ(){                                 //veamos si pide el Jugador y cuantas veces lo hace
   while (totalJ<17){
+    document.write("<br>Pide nueva carta el Jugado.")
     usadas++;                                     //se ha usado una nueva carta: usadas=usadas+1 
     manoJ[usadas-1] = naipes[usadas-1];           //tomamos la carta de la baraja y se la añadimos a la mano del Jugador
     totalJ=puntuar(manoJ);
